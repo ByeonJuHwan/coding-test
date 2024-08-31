f4a36d76fbcf5a02bb6d9cb491a5c0fd43f4ca0e
juhwan <asdcz@naver.com>
2024-08-31 13:49:44 +0900

    애너그램 -> 백준 1919 Math.abs = 절댓값 사용 함수

--- src/main/java/dev/be/codingtest/String/anagram.java
+++ src/main/java/dev/be/codingtest/String/anagram.java
@@ -25,5 +25,29 @@ public class anagram {
     예제 출력 : 8
      */
 
-    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        int[] countA = getAlphabetCount(a);
+        int[] countB = getAlphabetCount(b);
+
+        int answer = 0;
+
+        for(int i = 0; i < 26; i++)
+            answer += Math.abs(countA[i] - countB[i]); // 절댓값 함수
+
+        System.out.println(answer);
+    }
+
+    public static int[] getAlphabetCount(String str) {
+        int [] count = new int [26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++;
+        }
+        return count;
+    }
 }
