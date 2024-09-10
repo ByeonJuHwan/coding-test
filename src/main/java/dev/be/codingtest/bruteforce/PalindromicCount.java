b2e59a74ced25cbf6f69fd834a2b0942c0259261
juhwan <asdcz@naver.com>
2024-09-10 20:57:56 +0900

    백준 11005 -> 진법 변환2 경우의수 확인

---
+++ src/main/java/dev/be/codingtest/bruteforce/PalindromicCount.java
@@ -0,0 +1,51 @@
+package dev.be.codingtest.bruteforce;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class PalindromicCount {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int x = sc.nextInt();
+            boolean ans = false;
+            for (int i = 2; i <= 64; i++) {
+                int[] digit = convertBase(x, i);
+                if (isPalindrome(digit)) {
+                    ans = true;
+                    break;
+                }
+            }
+            System.out.println(ans ? "1" : "0");
+        }
+    }
+
+    private static boolean isPalindrome(int[] digit) {
+        for (int i = 0; i < digit.length / 2; i++) {
+            if(digit[i] != digit[digit.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] convertBase(int x, int B) {
+        int len = 0, copyX = x;
+        while (copyX > 0) {
+            copyX /= B;
+            len++;
+        }
+        int [] digit = new int[len];
+        len = 0;
+
+        while (x > 0) {
+            digit[len++] = x % B;
+            x /= B;
+        }
+
+        return digit;
+    }
+
+
+}
