8aa6309dbfcd29339d5a1dbda6d075427adbe373
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-23 08:18:43 +0900

    Title: 특정 원소들의 합, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241223/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int [][] arr = new int [4][4];
+
+        for(int i = 0; i<4; i++) {
+            for(int j = 0; j<4; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // 더하기
+        int sum = 0;
+
+        for(int i = 0; i<4; i++) 
+            for(int j = 0; j<= i ; j++)
+                sum += arr[i][j];
+            
+        
+        System.out.println(sum);
+
+    }
+}
