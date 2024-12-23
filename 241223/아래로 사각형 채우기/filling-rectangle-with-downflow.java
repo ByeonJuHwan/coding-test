fe541c2d6b28528b947ced16b1741ce1fc002b1d
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-23 16:15:49 +0900

    Title: 아래로 사각형 채우기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241223/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int [][] arr = new int [n][n];
+        
+        int num = 1;
+        for(int j = 0; j<n; j++) {
+            for(int i = 0; i<n; i++){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        sc.close();  // Scanner 객체를 닫아줍니다.
+    }
+}
