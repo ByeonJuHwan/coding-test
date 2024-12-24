4599bd86cbb538d283d4617fc3a72c40d5c86863
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-24 14:49:40 +0900

    Title: 격자 반대로 채우기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241224/격자 반대로 채우기/grid-anti-fill.java
+++ 241224/격자 반대로 채우기/grid-anti-fill.java
@@ -2,37 +2,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
+        int[][] arr = new int[n][n];
 
-        int [][] arr = new int [n][n];
-
-
-        // 오른쪽 아래에서 부터 채워서 지그재그
         int num = 1;
-        for(int j = n-1; j>=0; j--) {
-            if(j%2 == 0) {
-                for(int i = 0; i<n; i++) {
-                    arr[i][j] = num;
-                    num++;
+        for(int j = n-1; j >= 0; j--) {
+            if(j%2 == 0) { // 홀수 열
+                for(int i = 0; i < n; i++) {
+                    arr[i][j] = num++;
                 }
-            }
-            else {
-                for(int i = n-1; i>=0; i--) {
-                    arr[i][j] = num;
-                    num++;
+            } else { // 짝수 열
+                for(int i = n-1; i >= 0; i--) {
+                    arr[i][j] = num++;
                 }
             }
         }
 
-        for(int i = 0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 System.out.print(arr[i][j] + " ");
             }
-
             System.out.println();
         }
     }
