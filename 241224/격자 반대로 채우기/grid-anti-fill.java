d113046ff5eef6c203234a17a146bb858d7a1635
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-24 14:55:39 +0900

    Title: 격자 반대로 채우기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241224/격자 반대로 채우기/grid-anti-fill.java
+++ 241224/격자 반대로 채우기/grid-anti-fill.java
@@ -8,13 +8,27 @@ public class Main {
 
         int num = 1;
         for(int j = n-1; j >= 0; j--) {
-            if(j%2 == 0) { // 홀수 열
-                for(int i = 0; i < n; i++) {
-                    arr[i][j] = num++;
+
+            if(n % 2 == 0) {
+                if(j%2 == 0) { // 홀수 열
+                    for(int i = 0; i < n; i++) {
+                        arr[i][j] = num++;
+                    }
+                } else { // 짝수 열
+                    for(int i = n-1; i >= 0; i--) {
+                        arr[i][j] = num++;
+                    }
                 }
-            } else { // 짝수 열
-                for(int i = n-1; i >= 0; i--) {
-                    arr[i][j] = num++;
+            }
+            else {
+                if(j%2 == 1) { // 홀수 열
+                    for(int i = 0; i < n; i++) {
+                        arr[i][j] = num++;
+                    }
+                } else { // 짝수 열
+                    for(int i = n-1; i >= 0; i--) {
+                        arr[i][j] = num++;
+                    }
                 }
             }
         }
