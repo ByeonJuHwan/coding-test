2cdb5baa67edc41c99edff064326391c14081b5b
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-27 20:49:06 +0900

    Title: 동전이 있는 위치, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241227/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner (System.in);
+
+        // 격자의 크기
+        int n = sc.nextInt();
+
+        // 점들을 좀더 수월하게 찾기 위해서 1을 더한다
+        int [][] arr = new int [n+1][n+1];
+
+        // 동전의 개수
+        int m = sc.nextInt();
+
+
+        for(int i = 0; i<m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            arr[x][y] = 1;
+        }
+
+        for(int i = 1; i<n + 1; i++) {
+            for(int j = 1; j<n + 1; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
