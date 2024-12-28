486a5a926e177e221f0bab10f16b8dc99e04dc38
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-12-28 10:29:23 +0900

    Title: 공백을 기준으로 출력, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241228/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String [] arr1 = sc.nextLine().split(" ");
+        String [] arr2 = sc.nextLine().split(" ");
+
+        for(String s : arr1) {
+            System.out.print(s);
+        }
+
+        for(String s : arr2) {
+            System.out.print(s);
+        }
+    }
+}
