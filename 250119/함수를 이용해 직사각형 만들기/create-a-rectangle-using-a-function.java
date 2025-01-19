617308095c78aaa094cf38d8088489f8b59cb1e0
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2025-01-19 19:46:26 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 166ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250119/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        printNMLines(n,m);
+    }
+
+    public static void printNMLines(int n, int m) {
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
