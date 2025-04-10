d22d5e0fde1cf2320ec8fbd24af86e2c87ce5272
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2025-04-10 13:40:24 +0900

    Title: 상품 코드, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250410/상품 코드/product-code.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id2 = sc.next();
+        int code2 = sc.nextInt();
+        // Please write your code here.
+
+        System.out.println("product 50 is codetree");
+        System.out.println("product " + code2 + " is " + id2);
+    }
+}
