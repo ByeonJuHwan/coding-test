efb616be9e1d043c77efc068e3c9a437f139da4d
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2025-04-11 16:47:41 +0900

    Title: 총점 비교, Time: 226ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250411/총점 비교/compare-total-points.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student [] arr = new Student [n];
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int score1 = sc.nextInt();
+            int score2 = sc.nextInt();
+            int score3 = sc.nextInt();
+            arr[i] = new Student(name, score1, score2, score3);
+        }
+        // Please write your code here.
+
+        Arrays.sort(arr, (a,b) -> {
+            return (a.score1 + a.score2 + a.score3) - (b.score1 + b.score2 + b.score3);
+        });
+
+        for(Student s : arr) {
+            System.out.println(s.name + " " + s.score1 + " " + s.score2 + " " + s.score3);
+        }
+
+    }
+
+
+}
+
+class Student {
+    String name;
+    int score1;
+    int score2;
+    int score3;
+
+    public Student (String name, int score1, int score2, int score3) {
+        this.name = name;
+        this.score1 = score1;
+        this.score2 = score2;
+        this.score3 = score3;
+    }
+}
